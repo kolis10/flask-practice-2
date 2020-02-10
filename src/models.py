3029170f0908d1c441e5c43032f3cdbf3ff490e0
kolis10 <issacalleyne@gmail.com>
2020-02-10 16:41:27 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -2,6 +2,23 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(40))
+    last_name = db.Column(db.String(40))
+    date_of_birth = db.Column(db.String(10))
+    email = db.Column(db.String(120))
+
+    def __repr__(self):
+        return '<Person %r>' % self.username
+
+    def serialize(self):
+        return {
+            "first name": self.first_name,
+            "last name": self.last_name,
+            "date of birth": self.date_of_birth,
+            "email": self.email
+        }
 # class Person(db.Model):
 #     id = db.Column(db.Integer, primary_key=True)
 #     username = db.Column(db.String(80), unique=True, nullable=False)
